2ccbfb7a86262928d0e4c212de0c0ad999a3903a
Tyler Tolson <tylertolson1@gmail.com>
2019-10-08 09:38:58 -0400

    Added day 7

--- timeline.js
+++ timeline.js
@@ -3,7 +3,6 @@ $(() => {
 		const id = $(this).attr('id');
 		$('#Termine [id^=2019]').removeClass('ausgewählt');
 		$('#Monate li').removeClass('ausgewählt');
-
 		$(`#${id}`).addClass('ausgewählt');
 		$(`#Monate li#${id}`).addClass('ausgewählt');
 	});
@@ -14,20 +13,22 @@ $(() => {
 		$(`div.${id}`).addClass('selected');
 	});
 
-	const Wochen = 1;
+	const Wochen = 2;
 	const Tage = Wochen * 7;
+	$('[id^=Woche]').before('<br />');
 	$('[id^=Woche]').each(function() { inktoberImg($(this), Wochen, ['#Woche', '']); });
 	$('[id$=Inktober]').each(function() { inktoberImg($(this), Tage, ['#Tag-', '-Inktober']); });
-
-	// $('[id$=-Jahr]').click(function() { JahrVeränderung($(this)); });
+/* 	$('[id$=-Jahr]').click(function() {
+		const Datum = $(this).attr('id'); const JahreBekommen = [2019, 2020];
+		let Nummer = $('#Datum2019 num').html();
+		Nummer = parseInt($.trim(Nummer));
+		switch (Datum) {
+			case 'Bisherige-Jahr': if (JahreBekommen.includes(Nummer - 1)) { Nummer--; } break;
+			case 'Nächster-Jahr': if (JahreBekommen.includes(Nummer + 1)) { Nummer++; } break;
+		}
+		$('#Datum2019 num').html(Nummer);
+	}); */
 });
-
-function initialClass() {
-	$('#Monate li').addClass('abgewählt');
-	$('#2019jan').addClass('ausgewählt');
-	$('#Monate li#2019jan').addClass('ausgewählt');
-	$('#Monate li#2019jan').removeClass('abgewählt');
-}
 
 function inktoberImg(Standort, max, getPlace) {
 	let länge = 0;
@@ -36,16 +37,4 @@ function inktoberImg(Standort, max, getPlace) {
 		if (Standort.not(locationID).length) { länge++; }
 	}
 	if (länge === max) { Standort.css('display', 'none'); }
-}
-
-function JahrVeränderung(Standort) {
-	const Datum = Standort.attr('id');
-	const JahreBekommen = [2019, 2020];
-	let Nummer = $('#Datum2019 num').html();
-	Nummer = parseInt($.trim(Nummer));
-	switch (Datum) {
-		case 'Bisherige-Jahr': if (JahreBekommen.includes(Nummer - 1)) { Nummer--; } break;
-		case 'Nächster-Jahr': if (JahreBekommen.includes(Nummer + 1)) { Nummer++; } break;
-	}
-	$('#Datum2019 num').html(Nummer);
 }
